b2407e756426b33dca5aa2bfb331c49e2f8f9994
tjumma <tim@jumma.pro>
2021-06-15 13:11:31 +0300

    Unity Analytics

--- Assets/Scripts/_Main/Bootstrapper.cs
+++ Assets/Scripts/_Main/Bootstrapper.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 namespace Solcery
 {
@@ -6,6 +8,14 @@ namespace Solcery
     {
         void Start()
         {
+            AnalyticsEvent.GameStart();
+            var result1 = AnalyticsEvent.Custom("secret_found", new Dictionary<string, object>
+            {
+                { "secret_id", 123 },
+                { "time_elapsed", Time.timeSinceLevelLoad }
+            });
+            AnalyticsResult result = Analytics.CustomEvent("Test");
+            
             Application.targetFrameRate = 60;
 #if UNITY_WEBGL && !UNITY_EDITOR
             WebGLInput.captureAllKeyboardInput = true;
