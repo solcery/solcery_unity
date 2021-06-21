071b9d56de4d8a443e7cbdc4a7f6f6a89d342713
tjumma <tim@jumma.pro>
2021-06-21 22:26:44 +0300

    PVP state

---
+++ Assets/Scripts/Game/PVP.cs
@@ -0,0 +1,17 @@
+using Solcery.Utils;
+
+namespace Solcery
+{
+    public class PVP : Singleton<PVP>
+    {
+        public void Init()
+        {
+            
+        }
+
+        public void DeInit()
+        {
+            
+        }
+    }
+}
