d79b61602b433efbd3a3a4cf5e41d91595d9176d
tjumma <tim@jumma.pro>
2021-07-09 18:48:18 +0300

    fix CardDisplay PictureIndex bug

--- Assets/Scripts/UI/Create/CreateCard/UICardDisplay.cs
+++ Assets/Scripts/UI/Create/CreateCard/UICardDisplay.cs
@@ -6,7 +6,7 @@ namespace Solcery.UI.Create
 {
     public class UICardDisplay : MonoBehaviour
     {
-        public int CurrentPictureIndex => _currentIndex;
+        public int CurrentPictureIndex => _currentPictureIndex;
         public TMP_InputField CardNameInput => cardNameInput;
         public TMP_InputField CardDescriptionInput => cardDescriptionInput;
 
@@ -17,7 +17,7 @@ namespace Solcery.UI.Create
         [SerializeField] private TMP_InputField cardNameInput = null;
         [SerializeField] private TMP_InputField cardDescriptionInput = null;
 
-        private int _currentIndex = 0;
+        private int _currentPictureIndex = 0;
 
         public void Init()
         {
@@ -31,6 +31,7 @@ namespace Solcery.UI.Create
         {
             cardNameInput.text = metadata.Name;
             cardDescriptionInput.text = metadata.Description;
+            _currentPictureIndex = metadata.Picture;
             cardPicture.sprite = cardPictures.GetSpriteByIndex(metadata.Picture);
         }
 
@@ -50,21 +51,21 @@ namespace Solcery.UI.Create
         private void RandomizePicture()
         {
             var random = cardPictures.GetRandomSpriteIndex();
-            _currentIndex = random.Index;
+            _currentPictureIndex = random.Index;
             cardPicture.sprite = random.Sprite;
         }
 
         private void PrevPicture()
         {
-            var prev = cardPictures.GetPrevSpriteIndex(_currentIndex);
-            _currentIndex = prev.Index;
+            var prev = cardPictures.GetPrevSpriteIndex(_currentPictureIndex);
+            _currentPictureIndex = prev.Index;
             cardPicture.sprite = prev.Sprite;
         }
 
         private void NextPicture()
         {
-            var next = cardPictures.GetNextSpriteIndex(_currentIndex);
-            _currentIndex = next.Index;
+            var next = cardPictures.GetNextSpriteIndex(_currentPictureIndex);
+            _currentPictureIndex = next.Index;
             cardPicture.sprite = next.Sprite;
         }
     }
