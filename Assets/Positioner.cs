d5263af32147342feede455a796f05e3e59b3a34
tjumma <tim@jumma.pro>
2021-07-20 14:52:30 +0300

    DoTween

--- Assets/Positioner.cs
+++ Assets/Positioner.cs
@@ -1,13 +1,24 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 public class Positioner : MonoBehaviour
 {
     public RectTransform originRect;
-    public RectTransform goalRect;
+    public Canvas canvas;
+    public GameObject card;
+    public Transform cardTransform;
+    public Animator cardAnimator;
 
-    void Start()
+    async UniTask Start()
     {
-        goalRect.transform.position = originRect.transform.position;
-        Debug.Log(goalRect.anchoredPosition);
+        // goalRect.transform.position = originRect.transform.position;
+        // var start = goalRect.transform.position;
+        var cardClone = Instantiate(card, canvas.transform, true);
+        cardClone.GetComponent<Animator>().SetTrigger("Moving");
+        await cardClone.transform.DOMove(originRect.transform.position, 1f);
+        // await cardTransform.DOMove(start, 1f);
+        // transform.DOMove(new Vector3(1,2,3), 1);
+        // Debug.Log(goalRect.anchoredPosition);
     }
 }
