1dc16577cfb4b4cb0c37486c47c8b5bf52ac95b6
tjumma <tim@jumma.pro>
2021-07-26 17:41:03 +0300

    LogAppllyer

---
+++ Assets/LogApplyer.cs
@@ -0,0 +1,14 @@
+using Solcery.Utils;
+
+namespace Solcery
+{
+    public class LogApplyer : Singleton<LogApplyer>
+    {
+        public BoardData ApplyLogStep(BoardData origin, LogStepData logStep)
+        {
+            
+
+            return null;
+        }
+    }
+}
