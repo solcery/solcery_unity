0d5725f4890accc56aa636b990fcb7a55da8b51b
tjumma <tim@jumma.pro>
2021-08-13 10:47:22 +0300

    EditorMenu

--- Assets/Scripts/Editor/EditorMenu.cs
+++ Assets/Scripts/Editor/EditorMenu.cs
@@ -9,47 +9,48 @@ namespace Solcery.Editor
         [MenuItem("Solcery/Play", false, -1)]
         static async UniTask Play()
         {
-            await StopPlayingAndOpenScene("Assets/Scenes/_Main.unity");
+            await StopPlayingAndOpenScene("Assets/Scenes/Play.unity");
             EditorApplication.EnterPlaymode();
         }
 
-        [MenuItem("Solcery/Scene/Main", false, 21)]
-        static async UniTask OpenMainScene()
-        {
-            await StopPlayingAndOpenScene("Assets/Scenes/_Main.unity");
-        }
-
-        [MenuItem("Solcery/Scene/Menu", false, 22)]
-        static async UniTask OpenMenuScene()
-        {
-            await StopPlayingAndOpenScene("Assets/Scenes/Menu.unity");
-        }
-
-        [MenuItem("Solcery/Scene/Farm", false, 23)]
-        static async UniTask OpenFarmScene()
-        {
-            await StopPlayingAndOpenScene("Assets/Scenes/Farm.unity");
-        }
-
-        [MenuItem("Solcery/Scene/Create", false, 24)]
-        static async UniTask OpenCreateScene()
+        [MenuItem("Solcery/Create", false, -1)]
+        static async UniTask Create()
         {
             await StopPlayingAndOpenScene("Assets/Scenes/Create.unity");
+            EditorApplication.EnterPlaymode();
         }
 
-        [MenuItem("Solcery/Scene/Play", false, 25)]
+        [MenuItem("Solcery/Scene/Play", false, 1)]
         static async UniTask OpenPlayScene()
         {
             await StopPlayingAndOpenScene("Assets/Scenes/Play.unity");
         }
 
-        [MenuItem("Solcery/Scene/Test", false, 101)]
+        [MenuItem("Solcery/Scene/Lobby", false, 2)]
+        static async UniTask OpenLobbyScene()
+        {
+            await StopPlayingAndOpenScene("Assets/Scenes/Lobby.unity");
+        }
+
+        [MenuItem("Solcery/Scene/Game", false, 3)]
+        static async UniTask OpenGameScene()
+        {
+            await StopPlayingAndOpenScene("Assets/Scenes/Game.unity");
+        }
+
+        [MenuItem("Solcery/Scene/Create", false, 101)]
+        static async UniTask OpenCreateScene()
+        {
+            await StopPlayingAndOpenScene("Assets/Scenes/Create.unity");
+        }
+
+        [MenuItem("Solcery/Scene/Test", false, 201)]
         static async UniTask OpenTestScene()
         {
             await StopPlayingAndOpenScene("Assets/Scenes/Test/Test.unity");
         }
 
-        [MenuItem("Solcery/Scene/GUI Kit", false, 201)]
+        [MenuItem("Solcery/Scene/GUI Kit", false, 301)]
         static async UniTask OpenGUIKitScene()
         {
             await StopPlayingAndOpenScene("Assets/GUI Kit - Dark Geo/Scenes/DemoScene.unity");
