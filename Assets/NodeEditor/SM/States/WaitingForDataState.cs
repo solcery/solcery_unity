8facfe89b311c508fc8066b7e482e53794b88eac
tjumma <tim@jumma.pro>
2021-10-03 15:31:04 +0300

    event delay fix

--- Assets/NodeEditor/SM/States/WaitingForDataState.cs
+++ Assets/NodeEditor/SM/States/WaitingForDataState.cs
@@ -1,6 +1,5 @@
 // using Cysharp.Threading.Tasks;
 using Solcery.UI.NodeEditor;
-using Solcery.Utils.Reactives;
 using UnityEngine;
 
 namespace Solcery.NodeEditor.SM
@@ -12,11 +11,12 @@ namespace Solcery.NodeEditor.SM
         protected override void OnEnterState()
         {
             base.OnEnterState();
-            NodeEditorUnityToReact.Instance?.CallOnNodeEditorLoaded();
             UINodeEditor.Instance?.SetWaitingForData(true);
-            // Reactives.Subscribe(NodeEditor.Instance?.NodeEditorData, (nodeEditorData) => OnNodeEditorDataUpdate(nodeEditorData).Forget(), _stateCTS.Token);
+
             if (NodeEditor.Instance != null)
                 NodeEditor.Instance.OnNodeEditorDataChanged += OnNodeEditorDataUpdate;
+
+            NodeEditorUnityToReact.Instance?.CallOnNodeEditorLoaded();
         }
 
         private void OnNodeEditorDataUpdate(NodeEditorData nodeEditorData)
@@ -30,7 +30,6 @@ namespace Solcery.NodeEditor.SM
             brickConfigs?.PopulateFromData(nodeEditorData.BrickConfigsData);
             UINodeEditor.Instance?.SetWaitingForData(false);
             UINodeEditor.Instance.Init(nodeEditorData.BrickTree, true);
-            // await UniTask.WaitForEndOfFrame();
             stateMachine.Trigger("EditBrickTree");
         }
     }
